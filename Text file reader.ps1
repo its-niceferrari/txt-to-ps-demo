dc70889b407beab04592d2cd7070a8049646d7e6
Dominic <94198408+its-niceferrari@users.noreply.github.com>
2022-07-25 17:32:11 -0500

    Adding initial PowerShell script to repo

---
+++ Text file reader.ps1
@@ -0,0 +1,11 @@
+﻿Set-Location <# replace with path to parent directory #>
+Write-Output "Scanning contents . . ." `n
+Get-ChildItem <# replace with path to parent directory #> | ForEach-Object {
+    Write-Output "File address: "
+    Write-Output $_.FullName
+
+    Write-Output "File contents: "
+    Get-Content $_
+    Write-Output `n
+}
+Read-Host -Prompt "Press Enter to exit" #keeps PS console from automatically closing when finished
